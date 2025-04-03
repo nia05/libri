2b7c4295f39f6af8e54c5c4ed7647901919874ea
Marina <marinajocovic@gmail.com>
2025-04-03 19:15:46 +0200

    udpdate routes

--- libri/src/app/app.routes.ts
+++ libri/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginPageComponent } from './features/login/pages/login-page/login-page.component';
+import { MainComponent } from './core/components/main/main.component';
+
+export const routes: Routes = [
+    { path: 'login', component: LoginPageComponent},
+    { path: '', component: MainComponent}
+];
